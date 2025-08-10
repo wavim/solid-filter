d6e310a8b4061b54cecc2cb233a3f73e68e8b92e
carbonsoda <carbonsoda@outlook.com>
2025-08-10 12:48:19 +0800

    v1.0.1 package shake

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,8 @@
 import terser from "@rollup/plugin-terser";
 import wsolid from "rollup-preset-solid";
 
-export default wsolid({ plugins: [terser()] });
+export default wsolid({
+	input: "src/index.tsx",
+	output: { sourcemap: false, format: "es", dir: "dist" },
+	plugins: [terser()],
+});
